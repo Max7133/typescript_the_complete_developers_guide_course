50d0be22100ca701c2ab784688989ba166fa6ebb
Maksim Pasnitsenko <emexpas@outlook.com>
2023-10-03 17:39:42 +0300

    solving sorting issues in a bad way, union operators and Type Guards

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,7 +4,7 @@ class Sorter {
   //collection: number[];
 
   // added 'public' so then I won't need to specify the Field definition on the top and deleting the assignment inside the Constructor
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string) {
     //this.collection = collection;
   }
 
@@ -17,12 +17,29 @@ class Sorter {
     // therefore, I don't need to have to try to resort that again in the future
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // All of this only works if collection is a number[]
         // comparing a pair of Number each go round
         // if 'j' Element is greater it will swap those 2 Elements
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j]; // get's the reference of the Element on the LEFT hand side
-          this.collection[j] = this.collection[j + 1]; // takes the RIGHT hand side and throws it over to the LEFT
-          this.collection[j + 1] = leftHand; // takes the LEFT hand side and throws it over to the RIGHT
+        // Run this logic only if collection is an Array of Numbers
+        //// typeof - for primitive types such as 'number', 'string', 'boolean', 'symbol'
+        //// instanceof - is for whatever the 'constructor()' is for the type we are trying to check for
+        //// TYPE GUARD for whatever the 'constructor()' is for the Type we are trying to check for - restores access to one of the specific types that it's dealing with inside of a Union Operartor "|"
+        if (this.collection instanceof Array) {
+          // this.collection. - will show now only properties and methods for an Array (because of TypeScript and Type Guard)
+          // At this point collection === number[]
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j]; // get's the reference of the Element on the LEFT hand side
+            this.collection[j] = this.collection[j + 1]; // takes the RIGHT hand side and throws it over to the LEFT
+            this.collection[j + 1] = leftHand; // takes the LEFT hand side and throws it over to the RIGHT
+          }
+        }
+
+        // Only going to work if collection is a String
+        // If collection is a String, do this logic intead:
+        // ¬¬¬logic to compare and swap characters in a String
+        //// TYPE GUARD for Primitive Value - restores access to one of the specific types that it's dealing with inside of a Union Operartor "|"
+        if (typeof this.collection === 'string') {
+          // this.collection. - will show now only properties and methods for a String (because of TypeScript and Type Guard)
         }
       }
     }
