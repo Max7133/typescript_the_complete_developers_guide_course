58f19c93f40e1f43094ab812fa519ff54d0879f0
Maksim Pasnitsenko <emexpas@outlook.com>
2023-11-18 13:52:46 +0200

    enum & CsvFileReader class done

---
+++ stats/src/CsvFileReader.ts
@@ -0,0 +1,26 @@
+// for reading the contents of the CSV file (need to install npm install @types/node)
+import fs from 'fs'; // module in the Node Standard Library
+
+export class CsvFileReader {
+  data: string[][] = []; // starts as an empty Array
+
+  // public - automatically defining a Property on the Class "filename"
+  // whatever I pass in as the 1st Arg when I create a CsvFileReader, will be assigned to the "filename" Property on the Object instance
+  constructor(public filename: string) {}
+
+  read(): void {
+    // opens the file
+    // 1st Arg - the name of the file that I want to open 'football.csv'
+    // 2nd Arg - options Object with Flag 'encoding'
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8', // with this 'readFileSync, it can read any type of file, so an Image,executable, JSON file or CSV
+      })
+      .split('\n')
+      // for every row that I expect to be a String, it will return an Array of Strings
+      // it will 'map' over each individual string and 'split' on the comma, so at the end there will be a 2 Dimensional Array of Strings
+      .map((row: string): string[] => {
+        return row.split(',');
+      });
+  }
+}
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,27 +1,25 @@
-// for reading the contents of the CSV file (need to install npm install @types/node)
-import fs from 'fs'; // module in the Node Standard Library
+import { CsvFileReader } from './CsvFileReader';
 
-// 1st Arg - the name of the file that I want to open 'football.csv'
-// 2nd Arg - options Object with Flag 'encoding'
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8', // with this 'readFileSync, it can read any type of file, so an Image,executable, JSON file or CSV
-  })
-  .split('\n')
-  // for every row that I expect to be a String, it will return an Array of Strings
-  // it will 'map' over each individual string and 'split' on the comma, so at the end there will be a 2 Dimensional Array of Strings
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
+reader.read(); // read the file definition and assign it to 'data' Property
+
+// enum - enumeration
+// enum - because it's good use for a Collection of closly related Values
+// type: MatchResult
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
 
 let manUnitedWins = 0;
 // iterating through collection of matches
-for (let match of matches) {
+for (let match of reader.data) {
   // MU won as home team
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
     // MU won as away team
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
