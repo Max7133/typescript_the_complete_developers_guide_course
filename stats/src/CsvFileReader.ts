a228d9b6249bc536002789c4b17169eed9df8e21
Maksim Pasnitsenko <emexpas@outlook.com>
2023-12-05 12:03:07 +0200

    refactored MatchReader

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,20 +1,12 @@
 // for reading the contents of the CSV file (need to install npm install @types/node)
 import fs from 'fs'; // module in the Node Standard Library
 
-// 'CsvFileReader' is now a Generic Class, whenever I use this 'class' in any way, I'm going to have to pass in the Type to customize 'CsvFileReader'
-// that means that, when I use 'CsvFileReader', I'm going to pass in for instance 'MatchData' Tuple
-// and then visually instead of 'T' there will be 'MatchData'
-export abstract class CsvFileReader<T> {
-  data: T[] = []; // starts as an empty Array
+export class CsvFileReader {
+  data: string[][] = []; // starts as an empty Array
 
   // public - automatically defining a Property on the Class "filename"
   // whatever I pass in as the 1st Arg when I create a CsvFileReader, will be assigned to the "filename" Property on the Object instance
   constructor(public filename: string) {}
-
-  // helper function (marking it as 'abstract' that's going to be implemented by the Child Class)
-  // takes one row and converts the information inside of it and then return it
-  // takes in a single 'row' that is going to be an Array of Strings and it's going to return a 'MatchData' Tuple
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     // opens the file
@@ -30,10 +22,6 @@ export abstract class CsvFileReader<T> {
       // it will 'map' over each individual string and 'split' on the comma, so at the end there will be a 2 Dimensional Array of Strings
       .map((row: string): string[] => {
         return row.split(',');
-      })
-      // go through each individual 'row' and convert each 'value' inside of each of those 'rows' into correct type
-      // row - 1 of the single rows
-      // taking each value of 'row' of Strings and do some processing
-      .map(this.mapRow); // passing in a reference to 'mapRow'
+      });
   }
 }
--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,25 +1,41 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
 import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
 
-// Tuple
+// Tuple Definition
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-// at CsvFileReader I inserted the Generic Notation <MatchData> - Type that I want to be applied to <T> inside of 'CsvFileReader.ts'
-// In this case I'm customizing the 'CsvFileReader.ts' over inside the 'MatchReader.ts' (give me a copy of 'MatchReader' that's going to extend 'CsvFileReader' that's have been customized to work with 'MatchData')
-export class MatchReader extends CsvFileReader<MatchData> {
-  // takes a single 'row' of Array of Strings, returns something related to MatchData
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      // MatchResult (Enum Type)
-      // Type Assertion (overwriting TS default behavior) output: 'H', 'A', 'D'
-      row[5] as MatchResult,
-      row[6],
-    ];
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
+
+export class MatchReader {
+  matches: MatchData[] = [];
+
+  // reader: DataReader; (Don't need this, because with using 'public' it will automatically define this as a Property on this 'class')
+  constructor(public reader: DataReader) {}
+
+  // the 'load' Method is going to load up all the information through the 'CsvFileReader' and then do the actual conversion process
+  load(): void {
+    // whatever 'reader' is given
+    this.reader.read(); // the entire 'loading process' (get the .csv file, parce all the info inside of it, and produce the 2 dimensional Array of strings)
+    // gives back a 'New 2 Dimensional Array', and then it turns them in to all appropriate Values
+    // assigning all that transformed data (2 Dim Arr) to 'this.matches' after the Mapping Operation
+    this.matches = this.reader.data.map((row: string[]): MatchData => {
+      // go through each individual 'row' and convert each 'value' inside of each of those 'rows' into correct type
+      // row - 1 of the single rows
+      // taking each value of 'row' of Strings and do some processing
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        // MatchResult (Enum Type)
+        // Type Assertion (overwriting TS default behavior) output: 'H', 'A', 'D'
+        row[5] as MatchResult,
+        row[6],
+      ];
+    });
   }
 }
