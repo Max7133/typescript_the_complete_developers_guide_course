8bb4bb0829f6603e8224ada720ded36183071994
Maksim Pasnitsenko <emexpas@outlook.com>
2024-01-06 16:46:40 +0200

    implemented generating HTML report functionality

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 // Create an Object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -17,7 +18,8 @@ matchReader.load();
 // 1st Arg - Instance of the Analyzer which is WinsAnalysis (need to pass there the Name of the Team that I want to find the number of WINS for)
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport()
+  //new ConsoleReport()
+  new HtmlReport()
 );
 
 summary.buildAndPrintReport(matchReader.matches); // Team Man United won 18 games
