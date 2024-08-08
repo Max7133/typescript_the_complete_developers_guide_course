ff5068d8634fb11efe0b214bde58cf6fbe750738
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-08 11:07:25 +0300

    implemented eventMap for watching events on button click

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -3,6 +3,19 @@ export class UserForm {
   // used constructor so it can initialize and declare it at the same time
   constructor(public parent: Element) {}
 
+  // when is called, it will return an Object with some special Keys & Values
+  // : { [key: string]: () => void } - Tells TS that this will return an Object, with the Keys containing Strings, and that the Value for everything inside that Object will be a Function that takes no Arguments and returns nothing.
+  eventsMap(): { [key: string]: () => void } {
+    // the Keys inside of here are Strings which will contain first a Event Name after a colon, and then the Name of the Element/Selector for the Element that I want to bind the Event handler to
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there');
+  }
+
   template(): string {
     return `
       <div>
