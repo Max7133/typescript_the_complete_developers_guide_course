00b44c4c4e2a7cfdaa3ff126f3ee34c682846f26
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-08 11:40:51 +0300

    binded and tested event handlers

--- web/dist/index.b71e74eb.js
+++ web/dist/index.b71e74eb.js
@@ -608,19 +608,52 @@ class UserForm {
     constructor(parent){
         this.parent = parent;
     }
+    // when is called, it will return an Object with some special Keys & Values
+    // : { [key: string]: () => void } - Tells TS that this will return an Object, with the Keys containing Strings, and that the Value for everything inside that Object will be a Function that takes no Arguments and returns nothing.
+    eventsMap() {
+        // the Keys inside of here are Strings which will contain first a Event Name after a colon, and then the Name of the Element/Selector for the Element that I want to bind the Event handler to
+        return {
+            "click:button": this.onButtonClick,
+            "mouseenter:h1": this.onHeaderHover
+        };
+    }
+    onButtonClick() {
+        console.log("Hi there");
+    }
+    onHeaderHover() {
+        console.log("H1 was hovered over");
+    }
     template() {
         return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `;
+    }
+    bindEvents(fragment) {
+        const eventsMap = this.eventsMap();
+        for(let eventKey in eventsMap){
+            // eventName = 'click'
+            // selector = 'button'
+            const [eventName, selector] = eventKey.split(":");
+            // finds all the different Elements inside the 'fragment', that match that 'selector'
+            // so then it can iterate over that Array, and for every Element that will match, it will attach whatever Event handler it had referenced, in this caase 'onButtonClick'
+            // forEach for all those different Elements that it returns, set up Event Handler, 1st Arg watches for 'eventName', 2nd Arg Callback Function which is the Value inside - eventsMap[eventKey]
+            fragment.querySelectorAll(selector).forEach((element)=>{
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            });
+        }
     }
     // takes the 'template' and appends it as a Child to the Parent
     render() {
         const templateElement = document.createElement("template");
         // inserts innerHTML inside this 'template' element
         templateElement.innerHTML = this.template();
+        // passing in the DocumentFragment from the template 'element' that was created, which is 'content'
+        // DocumentFragment - reference to HTML that is ready to get inserted into the DOM
+        this.bindEvents(templateElement.content);
         this.parent.append(templateElement.content); // '.content' propertu is the actual HTML that I have stuck into the 'templateElement'
     }
 }
--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -9,6 +9,7 @@ export class UserForm {
     // the Keys inside of here are Strings which will contain first a Event Name after a colon, and then the Name of the Element/Selector for the Element that I want to bind the Event handler to
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
     };
   }
 
@@ -16,13 +17,35 @@ export class UserForm {
     console.log('Hi there');
   }
 
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Click Me</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      // eventName = 'click'
+      // selector = 'button'
+      const [eventName, selector] = eventKey.split(':');
+
+      // finds all the different Elements inside the 'fragment', that match that 'selector'
+      // so then it can iterate over that Array, and for every Element that will match, it will attach whatever Event handler it had referenced, in this caase 'onButtonClick'
+      // forEach for all those different Elements that it returns, set up Event Handler, 1st Arg watches for 'eventName', 2nd Arg Callback Function which is the Value inside - eventsMap[eventKey]
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   // takes the 'template' and appends it as a Child to the Parent
@@ -30,6 +53,10 @@ export class UserForm {
     const templateElement = document.createElement('template');
     // inserts innerHTML inside this 'template' element
     templateElement.innerHTML = this.template();
+
+    // passing in the DocumentFragment from the template 'element' that was created, which is 'content'
+    // DocumentFragment - reference to HTML that is ready to get inserted into the DOM
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content); // '.content' propertu is the actual HTML that I have stuck into the 'templateElement'
   }
